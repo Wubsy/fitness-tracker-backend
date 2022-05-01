d576533871eff9b82ac6a8f62f330a4f32047608
Christina <christina.hurt@purplewave.com>
2022-05-01 10:59:14 -0500

    login page

---
+++ fitnesslogin/validation/login.js
@@ -0,0 +1,25 @@
+const Validator = require("validator");
+const isEmpty = require("is-empty");
+module.exports = function validateLoginInput(data) {
+  let errors = {};
+// Convert empty fields to an empty string so we can use validator functions
+data.userName = !isEmpty(data.userName) ? data.userName : "";
+data.password = !isEmpty(data.password) ? data.password : "";
+
+// User Name checks
+if (Validator.isEmpty(data.userName)) {
+    errors.userName = "User Name field is required";
+  } else if (!Validator.isUserName(data.userName)) {
+    errors.userName = "User Name is invalid";
+  }
+
+// Password checks
+  if (Validator.isEmpty(data.password)) {
+    errors.password = "Password field is required";
+  }
+  
+return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};
